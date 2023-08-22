79cc8811ae9c5e499ba1068204f4814d98550cfa
RiyaG11 <riyagupta111201@gmail.com>
2023-08-22 23:49:37 +0530

    updated

--- backend/db/config.js
+++ backend/db/config.js
@@ -1,2 +1,2 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/e-com');
+mongoose.connect('mongodb+srv://user:qwerty123456789@cluster0.xjnqeku.mongodb.net/e-com');
--- backend/index.js
+++ backend/index.js
@@ -4,64 +4,65 @@ const cors = require('cors');
 const User = require('./db/user');
 const Product = require('./db/product');
 const Jwt = require('jsonwebtoken');
-const jwtKey = "e-com"; 
+const jwtKey = "e-com";
+const PORT = process.env.PORT || 3000
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.post('/register', async(req,res)=>{
+app.post('/register', async (req, res) => {
     let user = new User(req.body);
-    let result= await user.save();
+    let result = await user.save();
     result = result.toObject();
     delete result.password;
-    Jwt.sign({result},jwtKey, (err,token)=>{
-        if(err){
-            res.send({result: "Something went wrong, please try again later"})
+    Jwt.sign({ result }, jwtKey, (err, token) => {
+        if (err) {
+            res.send({ result: "Something went wrong, please try again later" })
         }
-        res.send({result, auth : token})
+        res.send({ result, auth: token })
     })
     // res.send(result);
     // console.log(result);
 })
 
-app.post('/login',async(req,res)=>{
+app.post('/login', async (req, res) => {
     console.log(req.body);
-    if(req.body.password && req.body.email){
+    if (req.body.password && req.body.email) {
         let user = await User.findOne(req.body).select("-password");
-        if(user){
-            Jwt.sign({user}, jwtKey, (err, token)=>{
-                if(err){
-                    res.send({result: "Something went wrong, Please try again later"})
+        if (user) {
+            Jwt.sign({ user }, jwtKey, (err, token) => {
+                if (err) {
+                    res.send({ result: "Something went wrong, Please try again later" })
                 }
-                res.send({user, auth:token})
+                res.send({ user, auth: token })
             })
-        }else{
-            res.send({result: "User not found"})
+        } else {
+            res.send({ result: "User not found" })
         }
-    }else{
-        res.send({result: "User not found"})
+    } else {
+        res.send({ result: "User not found" })
     }
 })
 
-app.post('/add', async(req,res)=>{
-    let product= new Product(req.body);
+app.post('/add', async (req, res) => {
+    let product = new Product(req.body);
     let result = await product.save();
     res.send(result);
     console.log(result);
 })
 
 
-app.get('/products', async (req,res)=>{
+app.get('/products', async (req, res) => {
     let products = await Product.find();
-   /* if(products.length>0){
-        res.send(products)
-    }else{
-        res.send({result: "No products found"})
-    }*/
+    /* if(products.length>0){
+         res.send(products)
+     }else{
+         res.send({result: "No products found"})
+     }*/
     res.send(products)
 })
 
-app.delete('/products/:id', async(req,res)=>{
-    const result = await Product.deleteOne({_id:req.params.id});
+app.delete('/products/:id', async (req, res) => {
+    const result = await Product.deleteOne({ _id: req.params.id });
     res.send(result);
 
 })
@@ -84,9 +85,9 @@ app.delete('/products/:id', async(req,res)=>{
 // })
 
 
-app.put('/products/:id',async(req,res)=>{
+app.put('/products/:id', async (req, res) => {
     let result = await Product.updateOne(
-        {_id: req.params.id},
+        { _id: req.params.id },
         {
             $set: req.body
         }
@@ -99,28 +100,28 @@ app.put('/products/:id',async(req,res)=>{
 
 
 
-app.get('/search/:key', async(req,res)=>{
+app.get('/search/:key', async (req, res) => {
     let result = await Product.find({
-        "$or":[
-            {name : {$regex : req.params.key}},
-           {category : {$regex : req.params.key}},
-            {brand : {$regex : req.params.key}},
-             {price : {$regex : req.params.key}}
+        "$or": [
+            { name: { $regex: req.params.key } },
+            { category: { $regex: req.params.key } },
+            { brand: { $regex: req.params.key } },
+            { price: { $regex: req.params.key } }
 
         ]
     })
     res.send(result);
 })
-app.get('/profile/:id', async(req,res)=>{
-    let result = await User.findOne({_id:req.params.id});
-    if(result){
+app.get('/profile/:id', async (req, res) => {
+    let result = await User.findOne({ _id: req.params.id });
+    if (result) {
         res.send(result);
-    }else{
-        res.send({result:"No record found"})
+    } else {
+        res.send({ result: "No record found" })
     }
-    
-    })
-app.listen(3000)
+
+})
+app.listen(PORT)
 // const dbConnect= async()=>{
 
 
